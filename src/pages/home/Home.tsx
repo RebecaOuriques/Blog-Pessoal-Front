24b350dfd771bfdf20389e0b646fe26654fd8fd8
Rebeca Ouriques <ouriques.rebecah@gmail.com>
2024-08-14 15:34:53 -0300

    Criação do crud de postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import homeLogo from '../../assets/homeLogo.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 function Home() {
   return (
-    <>
+      <>
       <div className="bg-indigo-900 flex justify-center">
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
@@ -12,17 +14,18 @@ function Home() {
             <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
-
-              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-            </div>
+            <ModalPostagem />
+            <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+          </div>
           </div>
 
           <div className="flex justify-center ">
-           <img src={homeLogo} alt="" className='w-2/3' />
+            <img src={homeLogo} alt="" className='w-2/3' />
+    
           </div>
         </div>
       </div>
-
+      <ListaPostagens />
     </>
   );
 }
